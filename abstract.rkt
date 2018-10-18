c158eb2965de5916df1407fc118a08a726a13bb4
unknown <linhchi.nguyen@eco-dlabe11.unitn.it>
2018-10-18 07:41:56 +0200

    abstract

--- abstract.rkt
+++ abstract.rkt
@@ -67,5 +67,116 @@
    (equal? (f1 -5.775) (f2 -5.775))))
 
 
+(require test-engine/racket-tests)
+; list of numbers -> list of numbers
+; converts a list of celcius
+; temperature to fahrenheit
+
+(define (cf* l)
+  (cond
+    [(empty? l) '()]
+    [else (cons
+           (C2F (first l))
+           (cf* (rest l)))]))
+
+; number -> number
+; converts one celcius 
+; temperature to fahrenheit
+(define (C2F c)
+  (+ (* 9/5 c) 32))
+
+; inventory -> list of strings
+; extracts the names of
+; toys from an inventory
+(define (names i)
+  (cond
+    [(empty? i) '()]
+    [else (cons
+           (IR-name (first i))
+           (names (rest i)))]))
+(define-struct IR [name price])
+; an ir is a structure:
+; (make-IR string number)
+; an inventory is one of:
+; - '()
+; - (cons IR inventory)
+
+(define (map1 k g)
+  (cond
+    [(empty? k) '()]
+    [else (cons 
+           (g (first k))
+           (map1 (rest k) g))]))
+
+; list of numbers -> list of numbers
+(define (cf*-from-map1 l) (map1 l C2F))
+; inventory -> list of strings
+(define (names-from-map1 i) (map1 i IR-name))
+
+(check-expect (cf* (list 100 0 -40))
+              '(212 32 -40))
+(check-expect (names
+               (list (make-IR "doll" 21) (make-IR "bear" 13)))
+              '("doll" "bear"))
+
+(check-expect (cf*-from-map1 '(100 0 -40)) '(212 32 -40))
+(check-expect (names-from-map1 (list (make-IR "doll" 21) (make-IR "bear" 13))) '("doll" "bear"))
+
+; number -> [list of number]
+
+; tabulates sin bw n and 0 (incl) in a list
+(define (tab-sin n)
+  (cond
+    [(zero? n) (list (sin 0))]
+    [else (cons (sin n)
+                (tab-sin (sub1 n)))]))
+
+; number-> list of number
+; tabulates sqrt bw n and 0 (incl] in a list
+(define (tab-sqrt n)
+  (cond
+    [(zero? n 0) (list (sqrt 0))]
+    [else (cons (sqrt n)
+                (tab-sqrt (sub1 n)))]))
+
+; [number -> number] number -> list of numbers
+
+(define (tabulate f n)
+  (cond
+    [(zero? n) (list (f 0))]
+    [else (cons (f n)
+                (tabulate f (sub1 n)))]))
+
+(define (tab-sqr n) (tabulate sqr n))
+(define (tab-tan n) (tabulate tan n))
+
+; [list of number] -> number
+; computes the sum of the numbers on l
+(define (sum l)
+  (cond
+    [(empty? l) 0]
+    [else (+ (first l)
+             (sum (rest l)))]))
+
+; [list of number] -> number
+; computes the product of the numbers on l
+(define (product l)
+  (cond
+   [(empty? l) 1]
+   [else (* (first l) 
+            (product (rest l)))]))
+
+; f[numbers -> number] init list-of-number > number
+(define (fold1 f init lon)
+  (cond
+    [(empty? l) init]
+    [else (f (first l) (fold1 f init (rest lon)))]))
+
+
+
+
+
+
+
 
 (test)
