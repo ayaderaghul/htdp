d0f2615a93df25cd39954cca833898a552d95f0d
unknown <linhchi.nguyen@soc-labo1-02.unitn.it>
2018-10-29 18:44:28 +0100

    done the main interpreter

--- bsl.rkt
+++ bsl.rkt
@@ -106,12 +106,15 @@
     [(add? ex) (make-add (subst (add-left ex) x v)
                          (subst (add-right ex) x v))]
     [(mul? ex) (make-mul (subst (mul-left ex) x v)
-                         (subst (mul-right ex) x v))]))
+                         (subst (mul-right ex) x v))]
+    [(list? ex) (map (lambda (el)
+                       (subst el x v)) ex)]))
 
 (check-expect (subst 'x 'x 3) 3)
 (check-expect (subst (make-add 'x 3) 'x 3) (make-add 3 3))
 (check-expect (subst (make-add (make-mul 'x 'x) 3) 'x 3)
               (make-add (make-mul 3 3) 3))
+(check-expect (subst (make-add 3 3) 'x 2) (make-add 3 3))
 
 (define (numeric? vex)
   (cond
@@ -180,10 +183,36 @@
 ; name exp
 ; fex sym sym fex -> value (number)
 (define (eval-definition1 ex f x b)
-  (eval-exp (subst b x (eval-exp ex)))) 
+  (cond
+    [(number? ex) ex]
+    [(add? ex) (+ (eval-definition1 (add-left ex) f x b)
+                  (eval-definition1 (add-right ex) f x b))]
+    [(mul? ex) (* (eval-definition1 (mul-left ex) f x b)
+                  (eval-definition1 (mul-right ex) f x b))]
+    [(list? ex) (eval-definition1
+                 (subst b x
+                        (eval-definition1
+                         (second ex) f x b))
+                 f x b)]))
+                  
+   
+  
+;  (eval-exp (subst b x (eval-exp ex)))) 
   
 (check-expect (eval-definition1 2 'f 'x (make-add 2 'x))
-              4)
+              2)
+(check-expect (eval-definition1 `(f ,(make-mul 1 3))
+                                'f 'x (make-add 2 'x))
+              5)
+(check-expect (eval-definition1
+               (make-add 1 `(f ,(make-mul 1 3)))
+               'f 'x (make-add 2 'x))
+              6)
+(check-expect (eval-definition1
+               (make-add `(f ,(make-mul 1 2))
+                         `(f ,(make-add 1 1)))
+               'f 'x (make-add 2 'x))
+              8)
 
 ;  (local
 ;    ((define value (eval-definition1 arg f x b))
@@ -208,36 +237,162 @@
 ; bsl-fun-def* symbol -> bsl-fun-def
 ; retrieves the definition of f in da
 ; signals an error if there is none
-(check-expect (lookup-def da-fgh 'g) '(g y (f (make-mul 2 'y))))
+(check-expect (lookup-def da-fgh 'g) `(g y (f ,(make-mul 2 'y))))
 (define (lookup-def da f)
-  (assq f da))
+  (local ((define res (assq f da)))
+          (if res res (error "not found"))))
+(check-error (lookup-def da-fgh 'q))
 (define da-fgh
   (list
    (list 'f 'x (make-add 3 'x))
-   (list 'g 'y '(f (make-mul 2 'y)))
+   (list 'g 'y `(f ,(make-mul 2 'y)))
    (list 'h 'v (make-add '(f v) '(g v)))))
 
 (define (eval-function* ex da)
   (cond
     [(number? ex) ex]
-    [(list? ex)  
+    [(add? ex) (+ (eval-function* (add-left ex) da)
+                  (eval-function* (add-right ex) da))]
+    [(mul? ex) (* (eval-function* (mul-left ex) da)
+                  (eval-function* (mul-right ex) da))]
+    [(list? ex)
      (local
        ((define res (lookup-def da-fgh (first ex)))
         (define var (second res))
         (define body (last res)))
-       (eval-definition1 (second ex) (first ex) var body))]
-    [(add? ex) (+ (eval-function* (add-left ex) da)
-                  (eval-function* (add-right ex) da))]
-    [(mul? ex) (* (eval-function* (add-left ex) da)
-                  (eval-function* (add-right ex) da))]))
+     (eval-function*
+      (subst body var (eval-function* (second ex) da))
+      da))]
+   ))
           
     
 (define e (make-add '(f 1) '(g 3)))
+(check-error (eval-function* '(q 2)))
 (check-expect (eval-function* 
                (make-add '(f 1) '(g 3)) da-fgh)
               (eval-exp (make-add (make-add 3 1) 
                                   (make-add 3 (make-mul 2 3)))))
       
+(check-expect (eval-function*
+               (make-add `(f ,(make-mul 1 3))
+                         `(h ,(make-add 2 2))) da-fgh)
+              (eval-exp (make-add
+                         (make-add 3 (make-mul 1 3))
+                         (make-add
+                          (make-add 3 (make-add 2 2))
+                          (make-add
+                           3
+                           (make-mul 2 (make-add 2 2)))))))
+
+;; mix
+
+;; bsl-da-all
+;; - '(symbol number)   constant def
+;; - '(symbol symbol bsl-da-all)
+
+(define (lookup-con-def alldef x)
+  (local ((define res (assq x alldef)))
+          (if res res (error "not found"))))
+
+(define (lookup-fun-def alldef f)
+  (local ((define res (assq f alldef)))
+          (if res res (error "not found"))))
+
+
+(define AD0
+  `((pi 3.14)
+    (area r ,(make-mul 'pi (make-mul 'r 'r)))
+    (vol r ,(make-mul 10 '(area r)))))
+
+(define (eval-all exp da)
+  (cond
+    [(number? exp) exp]
+    [(symbol? exp) (second (lookup-con-def da exp))]
+    [(add? exp)
+     (+ (eval-all (add-left exp) da)
+        (eval-all (add-right exp) da))]
+    [(mul? exp)
+     (* (eval-all (mul-left exp) da)
+        (eval-all (mul-right exp) da))]
+    [(list? exp)
+     (local
+       ((define res (lookup-fun-def da (first exp)))
+        (define var (second res))
+        (define body (last res)))
+       (eval-all
+        (subst body var
+               (eval-all (second exp) da))
+        da))]))
+       
+
+(check-within (eval-all 'pi AD0) 3.14 0.001)
+(check-within (eval-all '(area 2) AD0)
+              (eval-exp
+               (make-mul 3.14 (make-mul 2 2))) 0.001)
+(check-within (eval-all `(area ,(make-mul 2 4)) AD0)
+              (eval-exp
+               (make-mul 3.14 (make-mul 8 8))) 0.001)
+(check-within (eval-all `(vol ,(make-add 'pi 1)) AD0)
+              (eval-exp
+               (make-mul 10
+                         (make-mul
+                          3.14
+                          (make-mul 4.14 4.14)))) 0.001)
+(define (list-of-3? lst)
+  (= 3 (length lst)))
+(define (list-of-2? lst)
+  (= 2 (length lst)))
+(define (interpreter-h sexp sl)
+  (cond
+    [(number? sexp) sexp]
+    [(add? sexp)
+     (make-add
+      (interpreter-h (add-left sexp) sl)
+      (interpreter-h (add-right sexp) sl))]
+    [(mul? sexp)
+     (make-mul
+      (interpreter-h (mul-left sexp) sl)
+      (interpreter-h (mul-right sexp) sl))]
+    [(and (list? sexp) (list-of-3? sexp))
+     (cond
+       [(equal? (first sexp) '+)
+        (make-add (interpreter-h (second sexp) sl)
+                  (interpreter-h (third sexp) sl))]
+       [(equal? (first sexp) '*)
+        (make-mul (interpreter-h (second sexp) sl)
+                  (interpreter-h (third sexp) sl))])]
+                   
+    [(and (list? sexp) (list-of-2? sexp))
+     (local
+       ((define res (lookup-fun-def sl (first sexp)))
+        (define var (second res))
+        (define body (last res)))
+       (interpreter-h
+        (subst
+         body var
+         (interpreter-h (second sexp) sl)) sl))]
+    [(symbol? sexp) (second (lookup-con-def sl sexp))]
+  
+    [(list? sexp)
+     (map (lambda (se) (interpreter-h se sl)) sexp)]
+  ))
+
+(define (interpreter sexp sl)
+  (eval-all (interpreter-h sexp sl) sl))
+
+(check-within (interpreter 'pi AD0) 3.14 0.001)
+(check-within (interpreter '(area 2) AD0)
+              12.56 0.001)
+(check-within (interpreter '(area (+ 2 4)) AD0)
+              113.04 0.001)
+(check-within (interpreter '(vol (* pi 1)) AD0)
+              309.59144 0.001)
+
+
+
+
+
+
 
 
 
